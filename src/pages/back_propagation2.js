3dee4be4435a79216989b5473516c68399fcdc85
Antonio <thetonybl@gmailc.om>
2020-11-24 23:56:51 -0600

    Album con 3 practicas

---
+++ src/pages/back_propagation2.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const back_propagation2 = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default back_propagation2;
--- src/templates/album.js
+++ src/templates/album.js
@@ -1,90 +1,148 @@
-import React from "react";
-import AppBar from "@material-ui/core/AppBar";
-import Button from "@material-ui/core/Button";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import Container from "@material-ui/core/Container";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Grid from "@material-ui/core/Grid";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import CameraIcon from '@material-ui/icons/PhotoCamera';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+//import Link from '@material-ui/core/Link';
+import { Link } from "gatsby"
 
-import { graphql } from "gatsby";
-import Img from "gatsby-image";
 
-import Hero from "../components/Hero.js";
-import Footer from "../components/Footer.js";
-import BottomNav from "../components/BottomNav.js";
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://github.com/Bielma/algoritmos-ia">
+      IA Algorithms
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
-export default function Album(props) {
-  const { currentPage, numPages } = props.pageContext;
-  const prevPage = currentPage - 1 === 1 ? "/" : (currentPage - 1).toString();
-  const nextPage = (currentPage + 1).toString();
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+}));
 
-  const theme = useTheme();
+const data = [
+  {
+    id:1,
+    tittle: "Perceptron",
+    url: "https://github.com/Bielma/algoritmos-ia/tree/perceptron",
+    description: "Perceptron simple",
+    img: 'https://www.cs.utexas.edu/~teammco/misc/perceptron/perceptron.png',
+    page: '/perceptron/'
+  },
+  {
+    id:2,
+    tittle: "Adaline",
+    url: "https://github.com/Bielma/algoritmos-ia/tree/adaline",
+    description: "Adaline simple",
+    img: 'https://koldopina.com/wp-content/uploads/2018/04/Adaline.jpg',
+    page: '/adaline/'
+  },
+  {
+    id:3,
+    tittle: "BackPropagerion",
+    url: "https://github.com/Bielma/algoritmos-ia/tree/perceptron",
+    description: "NN para separar n clases",    
+    img: 'https://www.diegocalvo.es/wp-content/uploads/2017/07/perceptron-multicapa.png',
+    page: '/back_propagation/'
+  }
+];
 
-  const useStyles = makeStyles(theme => ({
-    icon: {
-      marginRight: theme.spacing(2)
-    },
-    button: {
-      margin: theme.spacing(1)
-    },
-    cardGrid: {
-      paddingTop: theme.spacing(8),
-      paddingBottom: theme.spacing(8)
-    },
-    card: {
-      height: "100%",
-      display: "flex",
-      flexDirection: "column"
-    },
-    cardMedia: {
-      paddingTop: "50%"
-    },
-    cardContent: {
-      flexGrow: 1
-    }
-  }));
-
+export default function Album() {
   const classes = useStyles();
 
   return (
-    <>
+    <React.Fragment>
       <CssBaseline />
       <AppBar position="relative">
         <Toolbar>          
           <Typography variant="h6" color="inherit" noWrap>
-            AI Algorithms
+            IA Algorithms
           </Typography>
         </Toolbar>
       </AppBar>
       <main>
-        {/*  */}
-        <Hero />
-        {/*  */}
+        {/* Hero unit */}
+        <div className={classes.heroContent}>
+          <Container maxWidth="sm">
+            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+              Inteligencia artificial
+            </Typography>
+            <Typography variant="h5" align="center" color="textSecondary" paragraph>
+              Perceptron, Adaline, BackPropagation...
+            </Typography>
+            <div className={classes.heroButtons}>
+              <Grid container spacing={2} justify="center">                          
+              </Grid>
+            </div>
+          </Container>
+        </div>
         <Container className={classes.cardGrid} maxWidth="md">
+          {/* End hero unit */}
           <Grid container spacing={4}>
-            {props.data.allFile.edges.map(card => (
-              <Grid item key={card.node.id} xs={12} sm={6} md={4}>
+            {data.map((card) => (
+              <Grid item key={card.id} xs={12} sm={6} md={4}>
                 <Card className={classes.card}>
-                  <Img fluid={card.node.childImageSharp.fluid} />
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image={card.img}
+                    title=  {card.tittle}
+                  />
                   <CardContent className={classes.cardContent}>
                     <Typography gutterBottom variant="h5" component="h2">
-                      {card.node.fields.exif.title}
+                      {card.tittle}
                     </Typography>
                     <Typography>
-                      <a href={card.node.fields.exif.copyright}>
-                        {card.node.fields.exif.description}
-                      </a>
+                      {card.description}
                     </Typography>
                   </CardContent>
                   <CardActions>
-                    <Button href={card.node.fields.exif.page} size="small" color="primary">
-                    {card.node.fields.exif.page}
-                    </Button>
+                    <Link to={card.page}>Ir</Link>
+
+                    <a href={card.url}>
+                       Código
+                    </a>            
+
                   </CardActions>
                 </Card>
               </Grid>
@@ -92,45 +150,22 @@ export default function Album(props) {
           </Grid>
         </Container>
       </main>
-      {/*  */}
-      <BottomNav
-        numPages={numPages}
-        prevPage={prevPage}
-        nextPage={nextPage}
-        currentPage={currentPage}
-        theme={theme}
-      />
-      {/*  */}
-      <Footer />
-    </>
+      {/* Footer */}
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+         Create by: 
+         Dany,
+         Ema,
+         Marce, 
+         Tony. 
+          
+        </Typography>
+        <Copyright />
+      </footer>
+      {/* End footer */}
+    </React.Fragment>
   );
 }
-
-export const pageQuery = graphql`
-  query allArtQuery($skip: Int!, $limit: Int!) {
-    allFile(
-      filter: { extension: { regex: "/(jpg)/" } }
-      limit: $limit
-      skip: $skip
-    ) {
-      edges {
-        node {
-          id
-          name
-          fields {
-            exif {
-              description
-              title
-              copyright
-            }
-          }
-          childImageSharp {
-            fluid(maxWidth: 2000, maxHeight: 1700, quality: 100) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-      }
-    }
-  }
-`;
